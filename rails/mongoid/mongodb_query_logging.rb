7c7acad18fd506d546f76f5c7da55e23c7bab826
Myunkyu Park <spica@wafflestudio.com>
2012-06-12 02:43:59 +0000

    change indent

--- rails/mongoid/mongodb_query_logging.rb
+++ rails/mongoid/mongodb_query_logging.rb
@@ -1,11 +1,11 @@
 # Rails.root/config/initializer/mongoid.rb
 Mongoid.configure do |config|
-	host = 'your host'
-	port = 27017 # by default 
+  host = 'your host'
+  port = 27017 # by default 
   dbname = 'your dbname'
   db_user_name = 'your db user name'
   db_password = 'your db password'
-	config.master = Mongo::Connection.new(host, port, :logger => Rails.logger).db(dbname)
-	# authenticate 필요하면 적용
+  config.master = Mongo::Connection.new(host, port, :logger => Rails.logger).db(dbname)
+  # authenticate 필요하면 적용
   config.master.authenticate(db_user_name, db_password)
 end
